1d1dee5dd3d701826eacdbc3dd8d9214fa6211e2
Jonathan Georgiou <j.georgiou94@gmail.com>
2022-04-04 14:14:38 +0100

    Remove error handling for now

--- clamav.go
+++ clamav.go
@@ -10,12 +10,12 @@ func main() {
     app  := "clamdscan /tmp/input/* --no-summary --fdpass --move=/tmp/virus"
 
     cmd := exec.Command("/bin/sh", "-c", app)
-    stdout, err := cmd.Output()
+    stdout, _ := cmd.Output()
 
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
+    // if err != nil {
+    //     fmt.Println(err.Error())
+    //     return
+    // }
 
     outputByLine := s.Split(string(stdout), "\n")
 
