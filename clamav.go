b74d997b6c99d31fb482a4653dd7016a7dfa8101
Jonathan Georgiou <j.georgiou94@gmail.com>
2022-04-04 14:11:56 +0100

    Formatting the returned string

--- clamav.go
+++ clamav.go
@@ -1,20 +1,34 @@
 package main
 
-import "fmt"
-import "os/exec"
+import (
+    "fmt"
+    "os/exec"
+    s "strings"
+)
 
 func main() {
     app  := "clamdscan /tmp/input/* --no-summary --fdpass --move=/tmp/virus"
 
     cmd := exec.Command("/bin/sh", "-c", app)
-    stdout, err := cmd.CombinedOutput()
+    stdout, err := cmd.Output()
 
     if err != nil {
         fmt.Println(err.Error())
         return
     }
 
+    outputByLine := s.Split(string(stdout), "\n")
+
     // Print the output
-    fmt.Println(string(stdout))
+    fmt.Println(string(outputByLine[0]))
+
+    for _, line := range outputByLine {
+        if s.Contains(line, "OK"){
+            fmt.Println("gotit")
+    }
+
+    
+        
+    }
 }
 
