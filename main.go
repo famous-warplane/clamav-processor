e0694ef924c1140dc78fdf39e5cc2d0502ef7b83
Jonathan Georgiou <j.georgiou94@gmail.com>
2022-04-05 14:35:26 +0100

    Try use command line arguments rather than flags

--- main.go
+++ main.go
@@ -2,40 +2,57 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"os"
 	"os/exec"
 	s "strings"
 
-	flag "github.com/jessevdk/go-flags"
+	// flag "github.com/jessevdk/go-flags"
 )
 
 func main() {
-	var opts  struct {
-		directoryToScan  string `short: "d" long: "dirscan" description: "Directory to be scanned by ClamAV" required: "true"`
-		quarantineDirectory string `short: "q" long: "quarantinedir" desription: "Directory to send files deemed dangerous by ClamAV to be quarantined" required: "true"`
-		safeDirectory string `short: "s" long: "safedir" desription: "Directory to send files deemed safe by ClamAV to be processed" required: "true"`
-	}
-	flag.Parse(opts)
+	// var opts struct {
+	// 	directoryToScan     string `short:"d" long:"dirscan" description:"Directory to be scanned by ClamAV" required:"true"`
+	// 	quarantineDirectory string `short:"q" long:"quarantinedir" desription:"Directory to send files deemed dangerous by ClamAV to be quarantined" required:"true"`
+	// 	safeDirectory       string `short:"s" long:"safedir" desription:"Directory to send files deemed safe by ClamAV to be processed" required:"true"`
+	// }
+	// args := []string {
+	// 	"-d",
+	// 	"-q",
+	// 	"-s",
+	// }
+	// flag.ParseArgs(&opts, args)
+	var directoryToScan string;
+	var quarantineDirectory string;
+	var safeDirectory string;
 
-	scanResultsArray := runClamdscan(opts.directoryToScan, opts.quarantineDirectory)
+	directoryToScan = os.Args[1]
+	quarantineDirectory = os.Args[2]
+	safeDirectory = os.Args[3]
+
+	scanResultsArray := runClamdscan(directoryToScan, quarantineDirectory)
 	// Look throgh the clamscan output for files marked OK
 	for _, line := range scanResultsArray {
-		if s.Contains(line, "OK") {
+		if s.Contains(line, "README") {
 			// Get the file path for the scanned files
-			filePath := s.Split(line, ":")[0]
+			filePath := s.Split(line, " ")[0]
 			// Move the files marked OK to a seperate directory
-			os.Rename(filePath, opts.safeDirectory)
+			// os.Rename(filePath, opts.safeDirectory)
+			fmt.Println(filePath + " hi " + safeDirectory)
 		}
 	}
 }
 
 func runClamdscan(scanDirectory string, quarantineDirectory string) []string {
-	clamdscanCmd := "clamdscan " + scanDirectory + " --no-summary --fdpass --move=" + quarantineDirectory
+	clamdscanCmd := "ls " + scanDirectory + " && ls " + quarantineDirectory
 
 	cmd := exec.Command("/bin/sh", "-c", clamdscanCmd)
-	stdout, _ := cmd.Output()
+	stdout, err := cmd.Output()
 
-	outputArray := s.Split(string(stdout), "\n")
-	return outputArray
+	if err != nil {
+		fmt.Println("ERROR! " + err.Error())
+	}
+
+	return s.Split(string(stdout), "\n")
 
 }
