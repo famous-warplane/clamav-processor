a9dcebf3eee23e985a15a2f41576228a2f3a2906
Dan Austin <dan.austin@hey.com>
2022-04-06 17:41:18 +0100

    feat: add scan interval. show clamav error output

--- config.go
+++ config.go
@@ -1,33 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"os"
+	"time"
 
 	"github.com/spf13/pflag"
 )
 
 type config struct {
 	inputPath, outputPath, quarantinePath string
+	scanInterval                          time.Duration
 }
 
 func initConfig() (config, error) {
 	conf := config{}
+	inPath := pflag.StringP("inputPath", "i", "", "path containing files to be scanned")
 	outPath := pflag.StringP("outputPath", "o", "", "directory to which safe scanned files are moved")
 	quarantinePath := pflag.StringP("quarantinePath", "q", "", "directory to which quarantined files are moved")
+	interval := pflag.IntP("period", "p", 60, "time in seconds between each scan")
+
 	pflag.Parse()
-	if len(os.Args) < 2 {
-		return conf, fmt.Errorf("no input directory provided. Usage: %s", UsageString)
+
+	conf.scanInterval = time.Duration(*interval) * time.Second
+
+	if *inPath == "" {
+		return conf, errors.New("no input directory provided")
 	}
 	conf.inputPath = os.Args[1]
 
 	if *outPath == "" {
-		return conf, fmt.Errorf("no output directory provided. Usage: %s", UsageString)
+		return conf, errors.New("no output directory provided")
 	}
 	conf.outputPath = *outPath
 
 	if *quarantinePath == "" {
-		return conf, fmt.Errorf("no quarantine directory provided. Usage: %s", UsageString)
+		return conf, errors.New("no quarantine directory provided")
 	}
 	conf.quarantinePath = *quarantinePath
 	return conf, nil
--- main.go
+++ main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"os/exec"
-	s "strings"
+	"strings"
+	"time"
 
+	"github.com/spf13/pflag"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
-
-const UsageString string = "cap INPUT_PATH -q QUARANTINE_PATH -o OUTPUT_PATH"
 
 var logger zap.SugaredLogger
 
 func initLogger() {
 	logConf := zap.NewProductionConfig()
 	logConf.Encoding = "console"
+	logConf.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
 	logConf.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	logConf.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	logConf.DisableStacktrace = true
 	unsugared, err := logConf.Build()
 
 	if err != nil {
@@ -33,43 +35,81 @@ func main() {
 	initLogger()
 	conf, err := initConfig()
 	if err != nil {
-		logger.Fatalf("failed to initialise application configuation: %v", err)
+		fmt.Println(err)
+		pflag.Usage()
+		os.Exit(1)
 	}
 
-	scanResultsArray := runClamdscan(conf.inputPath, conf.quarantinePath)
-	// Look throgh the clamscan output for files marked OK
+	ticker := time.NewTicker(conf.scanInterval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		logger.Debug("processing input path")
+		err = scan(conf)
+		if err != nil {
+			logger.Errorf("error while scanning directory: %v", err)
+		}
+	}
+}
+
+//scan runs a clamav scan and moves 'OK' files to the specified output path
+func scan(conf config) error {
+	logger.Debug("running scan")
+	scanResultsArray, err := runClamdscan(conf.inputPath, conf.quarantinePath)
+	if err != nil {
+		return err
+	}
+	logger.Info("scan complete")
+	logger.Info("processing 'OK' files")
 	for _, line := range scanResultsArray {
 		/*  The clamdscan outputs in a format similar to:
 			'/path/to/scanned/file: scanned OK'
 		so we search for lines that are OK in the output,
 		then split the line on ':' to extract just the filepath
 		*/
-		if s.Contains(line, "OK") {
-			outputParts := s.Split(line, ":")
+		if strings.Contains(line, "OK") {
+			outputParts := strings.Split(line, ":")
 			if len(outputParts) < 1 {
-				log.Printf("Failed to parse output line %q: Line does not contain expected character ':'", outputParts)
+				logger.Warnf("Failed to parse output line %q: Line does not contain expected character ':'", outputParts)
 				continue
 			}
 			filePath := outputParts[0]
-			filePathParts := s.Split(filePath, "/")
+			filePathParts := strings.Split(filePath, "/")
 			fileName := filePathParts[len(filePathParts)-1]
 
-			// Move the files marked OK to a seperate 'safe' directory
 			os.Rename(filePath, conf.outputPath+"/"+fileName)
 		}
 	}
+	return nil
 }
 
-func runClamdscan(scanDirectory string, quarantineDirectory string) []string {
+func runClamdscan(scanDirectory string, quarantineDirectory string) ([]string, error) {
 	clamdscanCmd := "clamdscan " + scanDirectory + " --fdpass  --no-summary --move=" + quarantineDirectory
 
 	cmd := exec.Command("/bin/sh", "-c", clamdscanCmd)
-	stdout, err := cmd.Output()
-
+	stdout, err := cmd.StdoutPipe()
+	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		fmt.Println("ERROR! " + err.Error())
+		return nil, fmt.Errorf("could not attach to command output: %w", err)
 	}
 
-	return s.Split(string(stdout), "\n")
+	err = cmd.Start()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start clamav scan: %w", err)
+	}
+
+	cmdOut, err := io.ReadAll(stdout)
+	if err != nil {
+		return nil, fmt.Errorf("failed to process scan output: %w", err)
+	}
+	cmdErr, err := io.ReadAll(stderr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to process scan output: %w", err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return nil, fmt.Errorf("clamav scan failed with error: %w: %s", err, strings.TrimSpace(string(cmdErr)))
+	}
+	return strings.Split(string(cmdOut), "\n"), nil
 
 }
