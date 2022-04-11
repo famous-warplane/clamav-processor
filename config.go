05838835789a502ab4af9e9b15f9e369abbf7267
Dan Austin <dan.austin@hey.com>
2022-04-11 15:46:50 +0100

    feat: add config file functionality

--- config.go
+++ config.go
@@ -2,41 +2,88 @@ package main
 
 import (
 	"errors"
-	"time"
+	"fmt"
 
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 type config struct {
-	inputPath, outputPath, quarantinePath string
-	scanInterval                          time.Duration
+	InputPath, OutputPath, QuarantinePath string
+	ScanInterval                          int
 }
 
+const (
+	inputPathName      string = "inputPath"
+	outputPathName     string = "outputPath"
+	quarantinePathName string = "quarantinePath"
+	scanIntervalName   string = "scanInterval"
+)
+
+var cfgFile string
+
 func initConfig() (config, error) {
-	conf := config{}
-	inPath := pflag.StringP("inputPath", "i", "", "path containing files to be scanned")
-	outPath := pflag.StringP("outputPath", "o", "", "directory to which safe scanned files are moved")
-	quarantinePath := pflag.StringP("quarantinePath", "q", "", "directory to which quarantined files are moved")
-	interval := pflag.IntP("period", "p", 60, "time in seconds between each scan")
+	setupFlags()
 
+	err := setupViper()
+	if err != nil {
+		return config{}, err
+	}
+
+	return populateConfig()
+}
+
+func setupFlags() {
+	pflag.StringVarP(&cfgFile, "config", "c", "", "path to a configuration file to be used (default is /etc/cap/cap.toml)")
+	pflag.StringP(inputPathName, "i", "", "the directory containing incoming files to scan")
+	pflag.StringP(outputPathName, "o", "", "the directory to which safe scanned files are moved")
+	pflag.StringP(quarantinePathName, "q", "", "the directory to which quarantined files are moved")
+	pflag.IntP("scanInterval", "s", 0, "the time interval at which the scan occurs")
 	pflag.Parse()
+}
 
-	conf.scanInterval = time.Duration(*interval) * time.Second
+func populateConfig() (config, error) {
+	conf := config{}
 
-	if *inPath == "" {
+	err := viper.Unmarshal(&conf)
+	if err != nil {
+		return conf, err
+	}
+
+	if conf.InputPath == "" {
 		return conf, errors.New("no input directory provided")
 	}
-	conf.inputPath = *inPath
 
-	if *outPath == "" {
+	if conf.OutputPath == "" {
 		return conf, errors.New("no output directory provided")
 	}
-	conf.outputPath = *outPath
 
-	if *quarantinePath == "" {
+	if conf.QuarantinePath == "" {
 		return conf, errors.New("no quarantine directory provided")
 	}
-	conf.quarantinePath = *quarantinePath
-	return conf, nil
 
+	return conf, nil
+}
+
+func setupViper() error {
+	viper.BindPFlags(pflag.CommandLine)
+
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath("/etc/cap")
+		viper.AddConfigPath(".")
+		viper.SetConfigName("cap")
+	}
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("Fatal error config file: %w \n", err)
+	}
+	logger.Infof("Using configuration file %q", viper.ConfigFileUsed())
+
+	viper.AutomaticEnv()
+
+	return nil
 }
